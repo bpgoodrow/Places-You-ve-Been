d54bd845d12ee4f207e6bb62feba29e1738f47a8
Ben Goodrow <bpgoodrow@gmail.com>
2022-06-13 16:10:11 -0700

    WIP adding interactivity Lesson 17
    Co-authored-by: Peter Vang <vang_peter06@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
-//Places youve been
-
 function PlacesYouveBeen() {
   this.destinations = {};
   this.currentId = 0;
-};
+}
 
 PlacesYouveBeen.prototype.addDestination = function (destination) {
   destination.id = this.assignId();
@@ -15,17 +13,14 @@ PlacesYouveBeen.prototype.assignId = function () {
   return this.currentId;
 };
 
-PlacesYouveBeen.prototype.findDestination = function(id) {
+PlacesYouveBeen.prototype.findDestination = function (id) {
   if (this.destinations[id] != undefined) {
     return this.destinations[id];
   }
   return false;
 };
 
-
-
-
-//Destination
+Destination
 
 function Destination(location, state, landmarks, bestTimeOfTheYear) {
   this.location = location;
@@ -34,10 +29,45 @@ function Destination(location, state, landmarks, bestTimeOfTheYear) {
   this.bestTimeOfTheYear = bestTimeOfTheYear;
 };
 
-let portlandOr = { location: "Portland", state: "Oregon", landmarks: "Waterfalls", bestTimeOfTheYear: "July" };
+//Usser Interface Logic
+let placeYouveBeen = new PlacesYouveBeen();
 
-let seattleWa = { location: "Seattle", state: "Washington", landmarks: "People", bestTimeOfTheYear: "July" };
+function displayDestinationDetails(placeYouveBeenToDisplay) {
+  let destinationsList = $("ul#destinations");
+  let htmlForDestinationInfo = "";
+  Object.keys(placeYouveBeenToDisplay).forEach(function (key) {
+    const destination = placeYouveBeenToDisplay.findDestination(key);
+    htmlForDestinationInfo += "<li id=" + destination.id + ">" + destination.location + " " + destination.state + "</li>";
+  });
+  destinationsList.html(htmlForDestinationInfo);
+}
 
-let portlandMe = { location: "Portland", state: "Maine", landmarks: "Trees", bestTimeOfTheYear: "October" };
+$(document).ready(function () {
+  $("form#new-destination").submit(function (event) {
+    event.preventDefault();
+    const inputLocation = $("input#new-location").val();
+    const inputState = $("input#new-state").val();
+    const inputLandmarks = $("input#new-landmarks").val();
+    const inputBestTimeOfTheYear = $("input#new-bestTimeOfTheYear").val();
+    let newDestination = new Destination(inputLocation, inputState, inputLandmarks, inputBestTimeOfTheYear);
+    placeYouveBeen.addDestination(newDestination);
+    console.log(placeYouveBeen.destinations);
 
-let north = { name: "North", location}
+  });
+});
+
+
+  // const results = Destination(location, state, landmarks, bestTimeOfTheYear);
+    // $("show-destination").text(results);
+
+// let portlandOr = { location: "Portland", state: "Oregon", landmarks: "Waterfalls", bestTimeOfTheYear: "July" };
+// let seattleWa = { location: "Seattle", state: "Washington", landmarks: "People", bestTimeOfTheYear: "July" };
+// let portlandMe = { location: "Portland", state: "Maine", landmarks: "Trees", bestTimeOfTheYear: "October" };
+
+// function Destination(portlandOr, seattleWa, portlandMe)
+
+// let portlandOr = { location: "Portland", state: "Oregon", landmarks: "Waterfalls", bestTimeOfTheYear: "July" };
+// let seattleWa = { location: "Seattle", state: "Washington", landmarks: "People", bestTimeOfTheYear: "July" };
+// let portlandMe = { location: "Portland", state: "Maine", landmarks: "Trees", bestTimeOfTheYear: "October" };
+// let placesYouveBeen = { name: "United States of America", cities: [portlandOr, seattleWa, portlandMe]};
+
